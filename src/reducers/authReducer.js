f6d203aad9307662ef30bbfbac7df192f91c43b9
Ivan David <davidacosta9704@gmail.com>
2021-05-28 19:10:32 -0500

    Update Frontend

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    cheking: true
+        //uid : null,
+        //name : null
+}
+
+export const authReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        default: 
+            return state;
+    }
+
+};
---
+++ src/types/types.js
@@ -0,0 +1,21 @@
+
+export const types = {
+    
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui  Close modal]',
+
+    eventSetActive: '[event] Set active',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+
+    authChecking: '[auth] checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout'
+
+}
